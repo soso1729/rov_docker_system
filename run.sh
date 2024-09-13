9485707871685a007afdb759aaa0349e5621bead
So Sato <satou5473@gmail.com>
2024-09-13 19:36:58 +0900

    feat : run.shにbletoothのダウンロード文を追加しました。

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "test"
+sudo apt update
+if ! dpkg -l | grep -q "^ii.*bluetooth"; then
+    echo "Bluetooth is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y bluetooth bluez
+else 
+    echo "Bluetooth is already installed."
+fi 
+
+echo "bash end"
