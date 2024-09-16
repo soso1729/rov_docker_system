cf7ef3c458c2ea2a939c80407f7bd8900c56945d
So Sato <satou5473@gmail.com>
2024-09-16 22:34:53 +0900

    feat : run.shの内容をdocker起動&ros1_noeticの設定してあるコンテナの起動のbashファイルにしました。

---
+++ userdir/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /root/userdir/src
+# Build directory: /root/userdir/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
