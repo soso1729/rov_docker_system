5bb71d59e39cab3c967f348a670142461f6d0bc6
So Sato <satou5473@gmail.com>
2024-09-17 02:22:45 +0900

    feat : 動作の前にgit pullを行うことでmainを最新のものにする機能を追加しました

--- run.sh
+++ run.sh
@@ -1,22 +1,27 @@
 #!/bin/bash  
 
 WORK_DIR="$(pwd)/userdir"
-LOCAL_DIR="/home"
 IMAGE_NAME="ros:noetic"
 CONTAINER_NAME="rov_ros_noetic"  
 HOST_NAME="rov_noetic"
 GUI_MODE=false
+GIT_URL="https://github.com/soso1729/rov_docker_system.git"
 
 sudo apt update
+git pull origin main
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         --GUI)
             GUI_MODE=true
+            CONTAINER_NAME="rov_ros_noetic_gui"
+            HOST_NAME="rov_noetic_gui"
             shift
             ;;
         --CUI)
             GUI_MODE=false
+            CONTAINER_NAME="rov_ros_noetic_cui"
+            HOST_NAME="rov_noetic_cui"
             shift
             ;;
         -w|--workspace)
@@ -59,13 +64,16 @@ else
         echo "Starting Docker container with GUI support (ROS Noetic)..."
         xhost +local:docker  
         docker run -it \
-            --name="${CONTAINER_NAME}_gui" \
+            --name="${CONTAINER_NAME}" \
             --hostname="$HOST_NAME" \
             --env="DISPLAY" \
             --env="QT_X11_NO_MITSHM=1" \
             --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
             --volume="$WORK_DIR:/root/userdir" \
             $IMAGE_NAME bash -c "
+
+            rsync -av --delete /root/userdir/ $WORK_DIR/
+
             # ROS Desktop Fullがインストールされていない場合にインストール
             if [ ! -d /opt/ros/noetic ]; then
                 echo 'Installing ros-noetic-desktop-full...'
@@ -86,6 +94,9 @@ else
                 echo 'ROS workspace already exists. Sourcing workspace...'
                 source /root/userdir/devel/setup.bash
             fi
+
+            trap 'rsync -av --delete /root/userdir/ $WORK_DIR/' EXIT
+
             bash
             "
         xhost -local:docker
@@ -94,10 +105,13 @@ else
     else
         echo "Starting Docker container in CUI mode (ROS Noetic)..."
         docker run -it \
-            --name="${CONTAINER_NAME}_cui" \
+            --name="${CONTAINER_NAME}" \
             --hostname="$HOST_NAME" \
             --volume="$WORK_DIR:/root/userdir" \
             $IMAGE_NAME bash -c "
+
+            rsync -av --delete /root/userdir/ $WORK_DIR/
+
             # ROS環境をセットアップ
             source /opt/ros/noetic/setup.bash
 
@@ -111,6 +125,9 @@ else
                 echo 'ROS workspace already exists. Sourcing workspace...'
                 source /root/userdir/devel/setup.bash
             fi
+
+            trap 'rsync -av --delete /root/userdir/ $WORK_DIR/' 
+            EXIT
 
             bash
             "
