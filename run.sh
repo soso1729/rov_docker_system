a2504cac8b51616e955d88615c0c9ceab760bc5d
So Sato <satou5473@gmail.com>
2024-09-20 02:50:02 +0900

    feat :dockerのインストールコマンドを追加しました。

--- run.sh
+++ run.sh
@@ -13,6 +13,35 @@ sudo apt update && sudo apt upgrade -y
 # 最新のソースコードを取得
 git pull origin main
 
+# Dockerのインストール確認
+if ! command -v docker &> /dev/null; then
+    
+    sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
+    sudo apt-get update
+
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release -y
+
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+    # Dockerサービスの起動と有効化
+    sudo systemctl start docker
+    sudo systemctl enable docker
+
+fi
+
 # オプション解析
 while [[ $# -gt 0 ]]; do
     case $1 in
