e9c726cdde20e90cb279e0783058390dc9a9e6b5
SO-SO <satou5473@gmail.com>
2024-09-24 17:50:54 +0900

    feat : humbleのコードを追加しました。

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash  
 
+# 初期設定
 WORK_DIR="$(pwd)/userdir"
 JOY_DIR="/dev/input"
+IMAGE_VER="noetic"
 IMAGE_NAME="ros:noetic"
 CONTAINER_NAME="rov_ros_noetic"  
 HOST_NAME="rov_noetic_cui"
@@ -16,31 +18,24 @@ git pull origin main
 
 # Dockerのインストール確認
 if ! command -v docker &> /dev/null; then
-    
     sudo apt-get remove docker docker-engine docker.io containerd runc -y
-
     sudo apt-get update
-
     sudo apt-get install \
         ca-certificates \
         curl \
         gnupg \
         lsb-release -y
-
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
     # Dockerサービスの起動と有効化
     sudo systemctl start docker
     sudo systemctl enable docker
-
 fi
 
 # オプション解析
@@ -48,14 +43,26 @@ while [[ $# -gt 0 ]]; do
     case $1 in
         --GUI)
             GUI_MODE=true
-            CONTAINER_NAME="rov_ros_noetic_gui"
-            HOST_NAME="rov_noetic_gui"
+            CONTAINER_NAME="rov_ros_${IMAGE_VER}_gui"
+            HOST_NAME="rov_${IMAGE_VER}_gui"
             shift
             ;;
         --CUI)
-            GUI_MODE=false
-            CONTAINER_NAME="rov_ros_noetic_cui"
-            HOST_NAME="rov_noetic_cui"
+            if [[ "$IMAGE_VER" != "humble" ]]; then
+                GUI_MODE=false
+                CONTAINER_NAME="rov_ros_${IMAGE_VER}_cui"
+                HOST_NAME="rov_${IMAGE_VER}_cui"
+            else
+                echo "Warning: --humble only supports GUI mode. Ignoring --CUI."
+            fi
+            shift
+            ;;
+        --humble)    
+            GUI_MODE=true
+            IMAGE_VER="humble"
+            IMAGE_NAME="ros:humble"
+            CONTAINER_NAME="rov_ros_${IMAGE_VER}_gui"
+            HOST_NAME="rov_${IMAGE_VER}_gui"
             shift
             ;;
         -w|--workspace)
@@ -74,11 +81,14 @@ while [[ $# -gt 0 ]]; do
             shift
             ;;
         *)
-            echo "Unknown option $1"
-            exit 1
+            POSITIONAL_ARGS+=("$1") # その他の引数を保存
+            shift
             ;;
     esac
 done
+
+# 保存した位置引数を復元
+set -- "${POSITIONAL_ARGS[@]}"
 
 # ROSワークスペースディレクトリの作成
 if [ ! -d "$WORK_DIR" ]; then
@@ -89,22 +99,21 @@ fi
 # 既存コンテナの確認
 container_exists=$(docker ps -a --filter "name=^/${CONTAINER_NAME}$" --format '{{.Names}}')
 
-# ROS NoeticのDockerイメージを取得
+# Dockerイメージを取得
 docker pull "$IMAGE_NAME"
 
-if [ "$container_exists" == "$CONTAINER_NAME" ]; then
-    xhost +local:docker
-    echo "Container $CONTAINER_NAME already exists. Starting it..."
-    docker start -ai "$CONTAINER_NAME"
-else
-    echo "Creating a new container $CONTAINER_NAME..."
+# Humble用のコンテナ作成と起動処理
+if [ "$IMAGE_VER" == "humble" ]; then
+    if [ "$container_exists" == "$CONTAINER_NAME" ]; then
+        xhost +local:docker
+        echo "Container $CONTAINER_NAME already exists. Starting it..."
+        docker start -ai "$CONTAINER_NAME"
+    else
+        echo "Creating a new container $CONTAINER_NAME (ROS 2 Humble)..."
 
-    ## コンテナ作成(GUI運用)
-    if [ "$GUI_MODE" = true ]; then
-
-        cd ~/rov_docker_system/userdir/src
-        git clone https://github.com/fredvaz/bluerov2.git
-        git clone https://github.com/arturmiller/uuv_simulator.git
+        # コンテナ作成(GUI運用のみ)
+        cd "$WORK_DIR/src"
+        git clone https://github.com/tasada038/ping_sonar_ros.git  
 
         # X11ディレクトリの確認
         if [ ! -d "/tmp/.X11-unix" ]; then
@@ -112,14 +121,14 @@ else
             exit 1
         fi
         
-        echo "Starting Docker container with GUI support (ROS Noetic)..."
+        echo "Starting Docker container with GUI support (ROS 2 Humble)..."
         
         xhost +local:docker
         if [ $? -ne 0 ]; then
             echo "Failed to set X11 permissions with xhost. Ensure X server is running and accessible."
             exit 1
         fi
-        
+
         docker run -it \
             --name="${CONTAINER_NAME}" \
             --hostname="$HOST_NAME" \
@@ -130,137 +139,200 @@ else
             --volume="$WORK_DIR:/root/userdir" \
             --device="$JOY_DIR:/dev/input" \
             $IMAGE_NAME bash -c "
+            
+            source /opt/ros/humble/setup.bash
 
-            roscore &
-
-            # OpenGLライブラリのインストール
-            apt update && apt install -y libgl1-mesa-glx libgl1-mesa-dri
-
-            # ROS環境をセットアップ
-            source /opt/ros/noetic/setup.bash
-
-
-            # ワークスペースが存在しなければビルド
-            if [ ! -f /root/userdir/devel/setup.bash ]; then
-                echo 'Setting up ROS workspace...'
+            # ワークスペースのセットアップ
+            if [ ! -f /root/userdir/install/setup.bash ]; then
+                echo 'Setting up ROS 2 Humble workspace...'
                 cd /root/userdir
-                catkin_make
-                source devel/setup.bash
+                mkdir -p src
+                colcon build
+                source install/setup.bash
             else
-                echo 'ROS workspace already exists. Sourcing workspace...'
-                source /root/userdir/devel/setup.bash
+                echo 'ROS 2 Humble workspace already exists. Sourcing workspace...'
+                source /root/userdir/install/setup.bash
             fi
-
-            echo 'keyboard-configuration keyboard-configuration/layoutcode string jp' | debconf-set-selections &&
-            echo 'keyboard-configuration keyboard-configuration/modelcode string pc105' | debconf-set-selections &&
-            echo 'keyboard-configuration keyboard-configuration/xkb-keymap select jp' | debconf-set-selections &&
-
 
             # 各パッケージのインストール
             apt update && apt install -y \
-            ros-noetic-gazebo-ros \
-            ros-noetic-gazebo-plugins \
-            ros-noetic-move-base \
-            ros-noetic-amcl \
-            ros-noetic-map-server \
-            ros-noetic-teb-local-planner \
-            ros-noetic-dwa-local-planner \
-            ros-noetic-global-planner \
-            ros-noetic-rviz \
-            ros-noetic-rqt \
-            ros-noetic-rqt-common-plugins \
-            ros-noetic-xacro \
-            ros-noetic-joy
-
-            ## シンボリックリンクの作成
-            if [ -L /tmp/rov_move ]; then
-                echo 'Existing symbolic link /tmp/rov_move found. Removing it.'
-                rm -f /tmp/rov_move
-            elif [ -d /tmp/rov_move ]; then
-                echo 'Existing directory /tmp/rov_move found. Removing it.'
-                rm -rf /tmp/rov_move
-            fi
-
-            ln -s ~/userdir/rov_move /tmp/rov_move
-
-            cd ~/userdir
-            catkin_make
+            ros-humble-rviz2 \
+            ros-humble-gazebo-ros-pkgs \
+            ros-humble-teleop-twist-keyboard \
+            ros-humble-joy
 
             bash
             "
         xhost -local:docker
+    fi
 
-    ## コンテナ作成(CUI運用)
+# Noetic用のコンテナ作成と起動処理
+else
+    if [ "$container_exists" == "$CONTAINER_NAME" ]; then
+        xhost +local:docker
+        echo "Container $CONTAINER_NAME already exists. Starting it..."
+        docker start -ai "$CONTAINER_NAME"
     else
+        echo "Creating a new container $CONTAINER_NAME..."
 
-        cd ~/rov_docker_system/userdir/src
-        git clone https://github.com/fredvaz/bluerov2.git
-        git clone https://github.com/arturmiller/uuv_simulator.git
+        ## コンテナ作成(GUI運用)
+        if [ "$GUI_MODE" = true ]; then
 
-        echo "Starting Docker container in CUI mode (ROS Noetic)..."
-        docker run -it \
-            --name="${CONTAINER_NAME}" \
-            --hostname="$HOST_NAME" \
-            --volume="$WORK_DIR:/root/userdir" \
-            --env="LIBGL_ALWAYS_SOFTWARE=1" \
-            --device="$JOY_DIR:/dev/input" \
-            $IMAGE_NAME bash -c "
+            cd ~/rov_docker_system/userdir/src
+            git clone https://github.com/fredvaz/bluerov2.git
+            git clone https://github.com/arturmiller/uuv_simulator.git
 
-            # OpenGLライブラリのインストール
-            apt update && apt install -y libgl1-mesa-glx libgl1-mesa-dri
-
-            # ROS環境をセットアップ
-            source /opt/ros/noetic/setup.bash
-
-            roscore &
+            # X11ディレクトリの確認
+            if [ ! -d "/tmp/.X11-unix" ]; then
+                echo "/tmp/.X11-unix does not exist. Ensure X server is running."
+                exit 1
+            fi
             
-            # ワークスペースが存在しなければビルド
-            if [ ! -f /root/userdir/devel/setup.bash ]; then
-                echo 'Setting up ROS workspace...'
-                cd /root/userdir
+            echo "Starting Docker container with GUI support (ROS Noetic)..."
+            
+            xhost +local:docker
+            if [ $? -ne 0 ]; then
+                echo "Failed to set X11 permissions with xhost. Ensure X server is running and accessible."
+                exit 1
+            fi
+            
+            docker run -it \
+                --name="${CONTAINER_NAME}" \
+                --hostname="$HOST_NAME" \
+                --env="DISPLAY=$DISPLAY" \
+                --env="QT_X11_NO_MITSHM=1" \
+                --env="LIBGL_ALWAYS_SOFTWARE=1" \
+                --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+                --volume="$WORK_DIR:/root/userdir" \
+                --device="$JOY_DIR:/dev/input" \
+                $IMAGE_NAME bash -c "
+                roscore &
+
+                # OpenGLライブラリのインストール
+                apt update && apt install -y libgl1-mesa-glx libgl1-mesa-dri
+
+                # ROS環境をセットアップ
+                source /opt/ros/noetic/setup.bash
+
+                # ワークスペースが存在しなければビルド
+                if [ ! -f /root/userdir/devel/setup.bash ]; then
+                    echo 'Setting up ROS workspace...'
+                    cd /root/userdir
+                    catkin_make
+                    source devel/setup.bash
+                else
+                    echo 'ROS workspace already exists. Sourcing workspace...'
+                    source /root/userdir/devel/setup.bash
+                fi
+
+                echo 'keyboard-configuration keyboard-configuration/layoutcode string jp' | debconf-set-selections &&
+                echo 'keyboard-configuration keyboard-configuration/modelcode string pc105' | debconf-set-selections &&
+                echo 'keyboard-configuration keyboard-configuration/xkb-keymap select jp' | debconf-set-selections &&
+
+                # 各パッケージのインストール
+                apt update && apt install -y \
+                ros-noetic-gazebo-ros \
+                ros-noetic-gazebo-plugins \
+                ros-noetic-move-base \
+                ros-noetic-amcl \
+                ros-noetic-map-server \
+                ros-noetic-teb-local-planner \
+                ros-noetic-dwa-local-planner \
+                ros-noetic-global-planner \
+                ros-noetic-rviz \
+                ros-noetic-rqt \
+                ros-noetic-rqt-common-plugins \
+                ros-noetic-xacro \
+                ros-noetic-joy
+
+                ## シンボリックリンクの作成
+                if [ -L /tmp/rov_move ]; then
+                    echo 'Existing symbolic link /tmp/rov_move found. Removing it.'
+                    rm -f /tmp/rov_move
+                elif [ -d /tmp/rov_move ]; then
+                    echo 'Existing directory /tmp/rov_move found. Removing it.'
+                    rm -rf /tmp/rov_move
+                fi
+
+                ln -s ~/userdir/rov_move /tmp/rov_move
+
+                cd ~/userdir
                 catkin_make
-                source devel/setup.bash
-            else
-                echo 'ROS workspace already exists. Sourcing workspace...'
-                source /root/userdir/devel/setup.bash
-            fi
 
-            echo 'keyboard-configuration keyboard-configuration/layoutcode string jp' | debconf-set-selections &&
-            echo 'keyboard-configuration keyboard-configuration/modelcode string pc105' | debconf-set-selections &&
-            echo 'keyboard-configuration keyboard-configuration/xkb-keymap select jp' | debconf-set-selections &&
+                bash
+                "
+            xhost -local:docker
 
+        ## コンテナ作成(CUI運用)
+        else
 
-            # 各パッケージのインストール
-            apt update && apt install -y \
-            ros-noetic-gazebo-ros \
-            ros-noetic-gazebo-plugins \
-            ros-noetic-move-base \
-            ros-noetic-amcl \
-            ros-noetic-map-server \
-            ros-noetic-teb-local-planner \
-            ros-noetic-dwa-local-planner \
-            ros-noetic-global-planner \
-            ros-noetic-rviz \
-            ros-noetic-rqt \
-            ros-noetic-rqt-common-plugins \
-            ros-noetic-xacro \
-            ros-noetic-joy
+            cd ~/rov_docker_system/userdir/src
+            git clone https://github.com/fredvaz/bluerov2.git
+            git clone https://github.com/arturmiller/uuv_simulator.git
 
-            ## シンボリックリンクの作成
-            if [ -L /tmp/rov_move ]; then
-                echo 'Existing symbolic link /tmp/rov_move found. Removing it.'
-                rm -f /tmp/rov_move
-            elif [ -d /tmp/rov_move ]; then
-                echo 'Existing directory /tmp/rov_move found. Removing it.'
-                rm -rf /tmp/rov_move
-            fi
+            echo "Starting Docker container in CUI mode (ROS Noetic)..."
+            docker run -it \
+                --name="${CONTAINER_NAME}" \
+                --hostname="$HOST_NAME" \
+                --volume="$WORK_DIR:/root/userdir" \
+                --env="LIBGL_ALWAYS_SOFTWARE=1" \
+                --device="$JOY_DIR:/dev/input" \
+                $IMAGE_NAME bash -c "
+                # OpenGLライブラリのインストール
+                apt update && apt install -y libgl1-mesa-glx libgl1-mesa-dri
 
-            ln -s ~/userdir/rov_move /tmp/rov_move
+                # ROS環境をセットアップ
+                source /opt/ros/noetic/setup.bash
 
-            cd ~/userdir
-            catkin_make
+                roscore &
+                
+                # ワークスペースが存在しなければビルド
+                if [ ! -f /root/userdir/devel/setup.bash ]; then
+                    echo 'Setting up ROS workspace...'
+                    cd /root/userdir
+                    catkin_make
+                    source devel/setup.bash
+                else
+                    echo 'ROS workspace already exists. Sourcing workspace...'
+                    source /root/userdir/devel/setup.bash
+                fi
 
-            bash
-            "
+                echo 'keyboard-configuration keyboard-configuration/layoutcode string jp' | debconf-set-selections &&
+                echo 'keyboard-configuration keyboard-configuration/modelcode string pc105' | debconf-set-selections &&
+                echo 'keyboard-configuration keyboard-configuration/xkb-keymap select jp' | debconf-set-selections &&
+
+                # 各パッケージのインストール
+                apt update && apt install -y \
+                ros-noetic-gazebo-ros \
+                ros-noetic-gazebo-plugins \
+                ros-noetic-move-base \
+                ros-noetic-amcl \
+                ros-noetic-map-server \
+                ros-noetic-teb-local-planner \
+                ros-noetic-dwa-local-planner \
+                ros-noetic-global-planner \
+                ros-noetic-rviz \
+                ros-noetic-rqt \
+                ros-noetic-rqt-common-plugins \
+                ros-noetic-xacro \
+                ros-noetic-joy
+
+                ## シンボリックリンクの作成
+                if [ -L /tmp/rov_move ]; then
+                    echo 'Existing symbolic link /tmp/rov_move found. Removing it.'
+                    rm -f /tmp/rov_move
+                elif [ -d /tmp/rov_move ]; then
+                    echo 'Existing directory /tmp/rov_move found. Removing it.'
+                    rm -rf /tmp/rov_move
+                fi
+
+                ln -s ~/userdir/rov_move /tmp/rov_move
+
+                cd ~/userdir
+                catkin_make
+
+                bash
+                "
+        fi
     fi
 fi
